0d865242b8bc0a9aefd9cf62656d14e0c2858c9e
Ramon Castan <ramon.castan175@gmail.com>
2017-09-04 15:34:02 -0300

    First Commit

---
+++ spring-demo-one/src/com/luv2code/springdemo/SpringHelloApp.java
@@ -0,0 +1,26 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringHelloApp {
+
+	public static void main(String[] args) {
+		//load the spring config file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+			
+		//retrieve a bean from the spring container
+		Coach theCoach = context.getBean("myCoach",Coach.class);
+
+		//call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//call new method for fortune service
+		System.out.println(theCoach.getDailyFortune());
+		
+		//close the context
+		context.close();
+	}
+
+}
---
+++ spring-demo-one/src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 5 hours on batting training";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
